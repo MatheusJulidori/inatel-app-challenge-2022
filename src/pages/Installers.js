e5f8559e49ab758b65131ed173fdee99fbd934ed
Matheus Julidori <matheus_julidori@yahoo.com.br>
2022-05-26 11:37:40 -0300

    Projeto inicio

---
+++ src/pages/Installers.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from "react";
+
+const Installers = () =>{
+
+    const [isLoading,setIsLoading] = useState(false)
+    const [installers,setInstallers] = useState([])
+    return(
+        
+        <></>
+    )
+}
+
+export default Installers
---
+++ src/pages/Plans.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from "react";
+
+const Plans = () =>{
+
+    const [isLoading,setIsLoading] = useState(false)
+    const [plans,setPlans] = useState([])
+    
+
+    return(
+        <></>
+    )
+}
+
+export default Plans

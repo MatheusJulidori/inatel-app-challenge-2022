44915a9fcb2f0ed2eabec9a9568337772ce89b08
Matheus Julidori <matheus_julidori@yahoo.com.br>
2022-05-26 16:57:37 -0300

    Installer List

---
+++ src/components/DefaultCard.js
@@ -0,0 +1,26 @@
+import {Card,CardHeader,CardActions} from '@mui/material';
+import  Typography  from '@mui/material/Typography';
+
+
+
+const DefaultCard = ({name,rating,price_per_km,id}) =>{
+
+
+  return (
+    <Card sx={
+      {maxWidth:345,
+        marginBottom:10,
+      backgroundColor:'#9ccd50'}
+    }>
+      <CardHeader
+        title={`${name}`}
+        subheader={`Price: $${price_per_km} per Km`}
+      />
+      <CardActions disableSpacing>
+        <Typography>Rating: {rating}/10</Typography>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default DefaultCard
--- src/pages/Installers.js
+++ src/pages/Installers.js
@@ -1,12 +1,49 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import Loading from '../components/Loading';
+import { Grid } from "@mui/material";
+import DefaultCard from '../components/DefaultCard'
 
-const Installers = () =>{
+const Installers = () => {
 
-    const [isLoading,setIsLoading] = useState(false)
-    const [installers,setInstallers] = useState([])
-    return(
-        
-        <></>
+    const [isLoading, setIsLoading] = useState(true)
+    const [installers, setInstallers] = useState([])
+
+    console.log(installers)
+
+    useEffect(() => {
+        axios.get('https://app-challenge-api.herokuapp.com/installers')
+            .then((response) => {
+                setInstallers(response.data)
+                setIsLoading(false)
+            })
+    }, [])
+
+    return (
+
+        <>
+            <Loading visible={isLoading}/>
+            <Grid container spacing={2} direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+                sx={{
+                    marginTop:'20px'
+                }}>
+            {
+                installers.map(item =>
+                    <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+                        <DefaultCard
+                            name={item.name}
+                            rating={item.rating}
+                            price_per_km={item.price_per_km}
+                            id={item.id}
+                        />
+                    </Grid>
+                )
+            }
+        </Grid>
+        </>
+
     )
 }
 
